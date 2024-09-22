20ed07d9a45e8b7cf039be090769317fb7465e86
Doszhan <dosikiliasov@gmail.com>
2024-09-23 00:18:46 +0500

    added and fixed validation and authentifised

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,5 +1,5 @@
 import { Link, useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FaEye } from "react-icons/fa";
 import { FaEyeSlash } from "react-icons/fa";
 
@@ -11,6 +11,15 @@ let Login = ()=>{
         email: null,
         password: null
     });
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [emailDirty, setEmailDirty] = useState(false)
+    const [passwordDirty, setPasswordDirty] = useState(false)
+    const [emailError, setEmailError] = useState("Email не может быть пустым")
+    const [passwordError, setPasswordError] = useState("Password не может быть пустым")
+    const [isValid, setIsValid] = useState(false);
+    const [loginError, setLoginError] = useState("")
+
     let signIn=()=>{
         fetch('http://localhost:8080/login', {
             method: 'POST',
@@ -21,6 +30,9 @@ let Login = ()=>{
         })
         .then(response => {
             if (!response.ok) {
+                if(response.status === 401 || response.status === 403 || response.status === 400){
+                    throw new Error("Неправильный логин или пароль")
+                }
                 throw new Error("Ошибка HTTP, статус " + response.status);
             }
             return response.json();
@@ -30,27 +42,77 @@ let Login = ()=>{
             navigate("/");
         })
         .catch(error => {
-            console.error("Произошла ошибка:", error);
+            setLoginError(error.message)
         });
     }
+    const blurHandler = (e) => {
+        switch(e.target.name){
+            case 'email': 
+                setEmailDirty(true)
+            case 'password':
+                setPasswordDirty(true) 
+        }
+    }
+    useEffect(()=>{
+        if(emailError || passwordError ) {
+            setIsValid(false)
+        }
+        else{
+            setIsValid(true)
+        }
+    },[emailError, passwordError])
+    
+    const emailHandler = (e) => {
+        setEmail(e.target.value);
+        setNewUser(prev => ({...prev, email: e.target.value}))
+        const re = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+
+        if (!re.test(String(e.target.value).toLowerCase())) {
+            setEmailError("Некорректный Email");
+        } else {
+            setEmailError('');
+        }
+    }
+    
+
+    const passwordHandler = (e) => {
+        setPassword(e.target.value)
+        setNewUser(prev => ({...prev, password: e.target.value}))
+        if(e.target.value.length < 6 || e.target.value.length > 15){
+            setPasswordError("Пароль должен быть длиннее 6 и меньше 15")
+            if(!e.target.value){
+                setPasswordError('Пароль не должен быть пустым')
+            }
+        }   
+        else{
+            setPasswordError('')
+        }
+    }
+
+
     return (
         <div className="registration">
             <form action='' className="form login">
                 <h1>Login</h1>
-                <input type="text" placeholder="Email" onChange={(e) => setNewUser({ ...newUser, email: e.target.value })} />
-                <input type={eyes ? "text" : "password"} placeholder="Password" onChange={(e) => setNewUser({ ...newUser, password: e.target.value })} />
-                {
-                    eyes ? (
-                        <span className="span_login" onClick={()=>(setEyes(false))}><FaEye /></span>
-                    )
-                    :
-                    (
-                        <span onClick={()=>(setEyes(true))}><FaEyeSlash /></span>
-                    )
+                {loginError&& <div style={{color: 'red', fontSize: '14px'}}>{loginError}</div>}
+                {(emailDirty && emailError) && <div style={{color: 'red', fontSize: '14px', display: 'flex', justifyContent: 'start', width: '60%'}}>{emailError}</div>}
+                <input type="text" onBlur={blurHandler} value={email} name="email" placeholder="Email" onChange={(e) => emailHandler(e)} />
+                {(passwordDirty &&passwordError) && <div style={{color: 'red', fontSize: '14px', display: 'flex', justifyContent: 'start', width: '65%', marginLeft: '15px'}}>{passwordError}</div>}
+                <div className='input_password'>
+                        <input className='input_svg' onBlur={ e => blurHandler(e)} value={password} name='password' type={eyes ? "text" : "password"} placeholder="Password" onChange={e => passwordHandler(e)} />
+                        {
+                            eyes ? (
+                                <span onClick={()=>(setEyes(false))}><FaEye /></span>
+                            )
+                            :
+                            (
+                                <span onClick={()=>(setEyes(true))}><FaEyeSlash /></span>
+                            )
 
-                   
-                }
-                <button type="button" className="btn" onClick={signIn}>Войти</button>
+                        
+                        }
+                    </div>
+                <button type="button" disabled={!isValid} className="btn" onClick={signIn}>Войти</button>
                 <Link to='/register'>Зарегистрироваться</Link>
             </form>
         </div>
--- src/Components/Registration/Registration.js
+++ src/Components/Registration/Registration.js
@@ -1,5 +1,5 @@
 import '../../App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { FaEye } from "react-icons/fa";
 import { FaEyeSlash } from "react-icons/fa";
 import { Link, useNavigate } from "react-router-dom";
@@ -7,15 +7,117 @@ import { Link, useNavigate } from "react-router-dom";
 let Registration =(props)=>{
     const navigate = useNavigate();
     const [eyes, setEyes] = useState(false);
-    const [image, setImage] = useState(null);
     const [newUser, setNewUser] = useState({
         email: null,
         password: null,
         name: null,
         surname: null,
-        phone: null,
-        urlImg: null,
+        phone: null
     })
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [name, setName] = useState("")
+    const [surname, setSurname] = useState("")
+    const [phone, setPhone] = useState('')
+    const [emailDirty, setEmailDirty] = useState(false)
+    const [passwordDirty, setPasswordDirty] = useState(false)
+    const [nameDirty, setNameDirty] = useState(false)
+    const [surnameDirty, setSurnameDirty] = useState(false)
+    const [phoneDirty, setPhoneDirty] = useState(false)
+    const [emailError, setEmailError] = useState("Email не может быть пустым")
+    const [passwordError, setPasswordError] = useState("Password не может быть пустым")
+    const [nameError, setNameError] = useState("Имя не может быть пустым")
+    const [surnameError, setSurnameError] = useState("Фамилия не может быть пустым")
+    const [phoneError, setPhoneError] = useState("Номер телефона не может быть пустым")
+    const [isValid, setIsValid] = useState(false)
+
+    const blurHandler = (e) => {
+        switch(e.target.name){
+            case 'email': 
+                setEmailDirty(true)
+            case 'password':
+                setPasswordDirty(true) 
+            case 'name': 
+                setNameDirty(true)
+            case 'surname':
+                setSurnameDirty(true)
+            case 'phone':
+                setPhoneDirty(true)
+        }
+    }
+
+
+    const emailHandler = (e) => {
+        setEmail(e.target.value);
+        setNewUser(prev => ({...prev, email: e.target.value}))
+        const re = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+
+        if (!re.test(String(e.target.value).toLowerCase())) {
+            setEmailError("Некорректный Email");
+        } else {
+            setEmailError('');
+        }
+    }
+    
+
+    const passwordHandler = (e) => {
+        setPassword(e.target.value)
+        setNewUser(prev => ({...prev, password: e.target.value}))
+        if(e.target.value.length < 6 || e.target.value.length > 15){
+            setPasswordError("Пароль должен быть длиннее 6 и меньше 15")
+            if(!e.target.value){
+                setPasswordError('Пароль не должен быть пустым')
+            }
+        }   
+        else{
+            setPasswordError('')
+        }
+    }
+
+    const nameHandler = (e) => {
+        setNewUser(prev => ({...prev, name: e.target.value}))
+        setName(e.target.value)
+        if(!e.target.value){
+            setNameError('Имя не должен быть пустым')
+        }
+        else{
+            setNameError('')   
+        }
+    }
+
+    const surNameHandler = (e) => {
+        if(!e.target.value){
+            setSurnameError('Фамилия не должно быть пустым')
+        }
+        else{
+            setSurnameError('')
+        }
+        setSurname(e.target.value)
+        setNewUser(prev => ({...prev, surname: e.target.value}))
+    }
+
+    const phoneHandler = (e) => {
+        setPhone(e.target.value)
+        setNewUser(prev => ({...prev, phone: e.target.value}))
+        const rePhone = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+
+        if(!rePhone.test(String(e.target.value).toLowerCase())){
+            setPhoneError("Некорректный номер телефона")
+        }
+        else{
+            setPhoneError('')
+        }
+    }
+
+    useEffect(()=>{
+        if(emailError || passwordError || nameError || surnameError || phoneError ) {
+            setIsValid(false)
+        }
+        else{
+            setIsValid(true)
+        }
+    },[emailError, passwordError, nameError, surnameError, phoneError])
+    
 
     let createToUser = ()=>{
         fetch('http://localhost:8080/register', {
@@ -46,24 +148,30 @@ let Registration =(props)=>{
                 <div className="registration_icon"><img src='./img/logo_reg.png'></img></div>
                 <form action='' className="form">
                     <h1>Регистрация</h1>
-                    <input type="text" placeholder="Email" onChange={(e) => setNewUser({ ...newUser, email: e.target.value })} />
-                    <input type={eyes ? "text" : "password"} placeholder="Password" onChange={(e) => setNewUser({ ...newUser, password: e.target.value })} />
-                    {
-                        eyes ? (
-                            <span onClick={()=>(setEyes(false))}><FaEye /></span>
-                        )
-                        :
-                        (
-                            <span onClick={()=>(setEyes(true))}><FaEyeSlash /></span>
-                        )
+                    {(emailDirty && emailError) && <div style={{color: 'red', fontSize: '14px', display: 'flex', justifyContent: 'start', width: '60%'}}>{emailError}</div>}
+                    <input onBlur={ e => blurHandler(e)} value={email} name='email' type="text" placeholder="Email" onChange={e => emailHandler(e)} />
+                    {(passwordDirty &&passwordError) && <div style={{color: 'red', fontSize: '14px', display: 'flex', justifyContent: 'start', width: '65%', marginLeft: '15px'}}>{passwordError}</div>}
+                    <div className='input_password'>
+                        <input className='input_svg' onBlur={ e => blurHandler(e)} value={password} name='password' type={eyes ? "text" : "password"} placeholder="Password" onChange={e => passwordHandler(e)} />
+                        {
+                            eyes ? (
+                                <span onClick={()=>(setEyes(false))}><FaEye /></span>
+                            )
+                            :
+                            (
+                                <span onClick={()=>(setEyes(true))}><FaEyeSlash /></span>
+                            )
 
-                    
-                    }
-                    <input type="text" placeholder="Имя" onChange={(e) => setNewUser({ ...newUser, name: e.target.value })} />
-                    <input type="text" placeholder="Фамилия" onChange={(e) => setNewUser({ ...newUser, surname: e.target.value })} />
-                    <input type="number" placeholder="Номер Телефона"  onChange={(e) => setNewUser({ ...newUser, phone: e.target.value })} />
-                    <input type="text" placeholder="Ваша картинка URL:"  onChange={(e) => setNewUser({ ...newUser, urlImg: e.target.value })} />
-                    <button type="button" className="btn" onClick={createToUser}>Создать аккаунт</button>
+                        
+                        }
+                    </div>
+                    {(nameDirty && nameError) && <div style={{color: 'red', fontSize: '14px', display: 'flex', justifyContent: 'start', width: '60%'}}>{nameError}</div>}
+                    <input onBlur={ e => blurHandler(e)} value={name} type="text" name='name' placeholder="Имя" onChange={e => nameHandler(e) } />
+                    {(surnameDirty && surnameError) && <div style={{color: 'red', fontSize: '14px', display: 'flex', justifyContent: 'start', width: '60%'}}>{surnameError}</div>}
+                    <input onBlur={ e => blurHandler(e)} value={surname} type="text" name='surname' placeholder="Фамилия" onChange={e => surNameHandler(e)} />
+                    {(phoneDirty && phoneError) && <div style={{color: 'red', fontSize: '14px', display: 'flex', justifyContent: 'start', width: '60%'}}>{phoneError}</div>}
+                    <input onBlur={ e => blurHandler(e)} value={phone} type="number" name='phone' placeholder="Номер Телефона" onChange={e => phoneHandler(e)}/>
+                    <button type="button" disabled={!isValid} className="btn" onClick={createToUser}>Создать аккаунт</button>
                     <Link to='/login'>У меня есть аккаунт</Link>
                 </form>
         </div>
